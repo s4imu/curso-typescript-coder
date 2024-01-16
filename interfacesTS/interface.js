1c6bac33e0e415e4cfec47b83b3c1412b8868ee6
Symon Barreto <symon@icomp.ufam.edu.br>
2024-01-16 01:04:35 -0400

    Interfaces

--- interfacesTS/interface.js
+++ interfacesTS/interface.js
@@ -1,3 +1,38 @@
 "use strict";
-function sayHello(pessoa) {
+function sayHello(person) {
+    console.log('Hello, ' + person.name);
 }
+function changeName(pessoa) {
+    person.name = 'Doug';
+}
+const person = {
+    name: 'Symon',
+    age: 25,
+    greet(lastName) {
+        console.log('Hello, my name is ' + this.name + ' ' + lastName);
+    }
+};
+sayHello(person);
+changeName(person);
+sayHello(person);
+// sayHello({name: 'John', age: 15, height: '175 cm'})
+person.greet("Barreto");
+//Using classes
+class Client {
+    constructor() {
+        this.name = '';
+        this.lastBuy = new Date;
+    }
+    greet(lastName) {
+        console.log('Hello, my name is ' + this.name + ' ' + lastName);
+    }
+}
+const myClient = new Client();
+myClient.name = 'Daniel';
+myClient.greet('Suzuki');
+console.log(myClient.lastBuy);
+let pow;
+pow = function (base, exp) {
+    return base ** exp;
+};
+console.log(pow(2, 4));
--- interfacesTS/interface.ts
+++ interfacesTS/interface.ts
@@ -1,3 +1,56 @@
-function sayHello(pessoa: any){
-
+interface Human {
+    name: string,
+    age?: number, //optional
+    [prop: string]: any, //atribute unknown name
+    greet(lastName: string): void
 }
+
+function sayHello(person: Human){
+    console.log('Hello, ' + person.name)
+}
+
+function changeName(pessoa: Human){
+    person.name = 'Doug'
+}
+
+const person = {
+    name:  'Symon',
+    age: 25,
+    greet(lastName: string) {
+        console.log('Hello, my name is ' + this.name + ' ' + lastName)
+    }
+}
+
+sayHello(person)
+changeName(person)
+sayHello(person)
+// sayHello({name: 'John', age: 15, height: '175 cm'})
+person.greet("Barreto")
+
+//Using classes
+class Client implements Human {
+    name:  string = ''
+    lastBuy: Date = new Date
+    greet(lastName: string) {
+        console.log('Hello, my name is ' + this.name + ' ' + lastName)
+    }
+}
+
+const myClient =  new Client()
+
+myClient.name =  'Daniel'
+myClient.greet('Suzuki')
+console.log(myClient.lastBuy)
+
+//Interface Function
+interface calcFunction {
+    (a: number, b: number): number
+}
+
+let pow: calcFunction
+
+pow = function(base: number, exp: number): number {
+    return base ** exp 
+}
+
+console.log(pow(2,4))
